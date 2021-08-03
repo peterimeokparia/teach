9a62387d312d4a6a4d2ce159a67e72341b82e945
peterimeokparia <68337147+peterimeokparia@users.noreply.github.com>
2021-08-03 17:07:03 -0400

    Delete src/Services/course directory

--- src/Services/course/Pages/OnlineQuestionsPage/Components/OnlineQuestionsCourseListPage/inlineStyles.js
+++
@@ -1,9 +0,0 @@
-export const helpIconStyle = () => {
-  return {
-    fontSize: 70,
-    position: "fixed",
-    "z-index": 1, 
-    left: "50%"
-    // top: "-3%",
-  };
-};
--- src/Services/course/Pages/SignUp/AccountVerificationForm/helpers/index.js
+++
@@ -1,5 +0,0 @@
-export const pageNavigationHelper = {
-    login: (operatorBusinessName) => `http://localhost:3000/${operatorBusinessName}/login`,
-    users: (operatorBusinessName) => `http://localhost:3000/${operatorBusinessName}/users`,
-    classRoom: (operatorBusinessName, classRoomId) => `/${operatorBusinessName}/LessonPlan/invite/userverified/classRoom/${classRoomId}`
-};

b3b1d8d108edced62c47f0cdd68c90ad4259b04c
peterimeokparia <68337147+peterimeokparia@users.noreply.github.com>
2021-08-03 16:57:42 -0400

    Delete src/services/course/pages/AttendancePage/Components directory

--- src/services/course/pages/AttendancePage/Components/MarkAttendanceComponent/helpers/index.js
+++
@@ -1,7 +0,0 @@
-export function markAttendanceForSelectedStudents( pushNotificationUsers, selectedCourseFromCourseDropDrown, attendance, markAttendanceAction ) {
-    attendance.selectedStudents.forEach(student => {
-       let attendanceData = { ...attendance, studentId: student?._id  };
-       
-       markAttendanceAction( student, selectedCourseFromCourseDropDrown, attendanceData, pushNotificationUsers );
-    });
-};
